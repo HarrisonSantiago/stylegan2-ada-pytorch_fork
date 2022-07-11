b4d9abd5995f40d784873b3ccbe234c2696469cd
HarrisonSantiago <harrisonsantiago4@gmail.com>
2022-07-11 16:49:36 -0700

    switched to jupyter one before

--- ILO_driver.py
+++ ILO_driver.py
@@ -155,20 +155,20 @@ class LatentOptimizer(torch.nn.Module):
         plt.title('Step 4, res: '+ str(start_res))
         plt.show()
 
-        print('comparing')
-        print(best_z[:15])
-        print(z_p[:15])
+        #print('comparing')
+        #print(best_z[:15])
+        #print(z_p[:15])
 
         return best_z, best_img, mse_min
 
     def step5(self, z_p_sq, z_k,  current_res, initial_learning_rate = 0.05):
         print('--- starting step 5 ---')
-        z_k = z_k.clone().detach()
-        holder = torch.tensor(z_k, dtype=torch.float32, device="cuda", requires_grad=True).cuda()
+        #z_k = z_k.clone().detach()
+        #holder = torch.tensor(z_k, dtype=torch.float32, device="cuda", requires_grad=True).cuda()
 
         num_steps = 60
 
-        #z = torch.randn([1, self.G.z_dim], dtype=torch.float32, device="cuda", requires_grad=True).cuda()
+        holder = torch.randn([1, self.G.z_dim], dtype=torch.float32, device="cuda", requires_grad=True).cuda()
 
         optimizer5 = torch.optim.Adam([holder], lr=initial_learning_rate)
 
