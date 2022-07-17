171de62ec39a077157d4bef86355ccd8cc7ad6ec
HarrisonSantiago <harrisonsantiago4@gmail.com>
2022-07-16 21:44:17 -0700

    Added alternate solver

--- ILO_driver.py
+++ ILO_driver.py
@@ -406,7 +406,7 @@ class LatentOptimizer(torch.nn.Module):
                     break
 
             targ_w = block_ws[i]
-            holder = torch.ones(block_ws[i].shape, device = "cuda", requires_grad = True)
+            holder = torch.ones(block_ws[i].shape, device = "cuda", requires_grad = True).cuda()
             holder = holder * block_ws[i].clone()
 
             optim = torch.optim.Adam([holder], lr = 0.05)
