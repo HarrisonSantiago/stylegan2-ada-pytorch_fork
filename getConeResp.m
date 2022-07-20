0320da04e384eb15256f2734549c66a69ebb0a68
HarrisonSantiago <harrisonsantiago4@gmail.com>
2022-07-19 22:10:29 -0700

    Added alternate solver

--- getConeResp.m
+++ getConeResp.m
@@ -10,7 +10,7 @@ function coneResponse = getConeResp(img_path)
     retina = ConeResponse('eccBasedConeDensity', true, 'eccBasedConeQuantal', true, ...
         'fovealDegree', 1.0, 'pupilSize', 2.5);
 
-    load("render.mat", "render")
+    %load("render.mat", "render")
 
     [~, ~, imageLinear, coneResponse] = retina.compute(double(imageRGB));
     %coneResponse = render * imageLinear(:);
