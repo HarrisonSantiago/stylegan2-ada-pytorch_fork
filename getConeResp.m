641c33bf02dbbb36601a49253f67aff69e87d363
HarrisonSantiago <harrisonsantiago4@gmail.com>
2022-07-19 22:05:30 -0700

    Added alternate solver

---
+++ getConeResp.m
@@ -0,0 +1,17 @@
+
+function coneResponse = getConeResp(img_path)
+
+    imageRGB = imread(img_path);
+    fprintf('%d ',size(imageRGB)')
+    
+    tbUseProject('ISETImagePipeline');
+    %display = displayCreate('CRT12BitDisplay');
+
+    retina = ConeResponse('eccBasedConeDensity', true, 'eccBasedConeQuantal', true, ...
+        'fovealDegree', 1.0, 'pupilSize', 2.5);
+
+    load("render.mat", "render")
+
+    [~, ~, imageLinear, coneResponse] = retina.compute(double(imageRGB));
+    %coneResponse = render * imageLinear(:);
+end 
