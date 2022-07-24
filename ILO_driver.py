6bb50e140a46f2685c9c4db7de9939d217385c27
HarrisonSantiago <harrisonsantiago4@gmail.com>
2022-07-24 12:40:01 -0700

    This and the last one changed datasettool

--- ILO_driver.py
+++ ILO_driver.py
@@ -540,7 +540,7 @@ class LatentOptimizer(torch.nn.Module):
 
                 # for MSELoss
                 loss = 0.5 * loss_fcn(gen_img[0], self.targ_img)
-                loss += loss_fcn1.forward(gen_img[0], self.targ_img)
+                loss += torch.squeeze(loss_fcn1.forward(gen_img[0], self.targ_img))
 
                 optimizer.zero_grad()
                 loss.backward()
