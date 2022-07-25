4b94b9dac9ecf88c8c41b53a97ce71d1de6c6d07
HarrisonSantiago <harrisonsantiago4@gmail.com>
2022-07-25 14:49:14 -0700

    Today I've been doing stuff with loss fcns

--- ILO_driver.py
+++ ILO_driver.py
@@ -67,7 +67,7 @@ class LatentOptimizer(torch.nn.Module):
         self.engine.cd(self.home_dir)
         self.retinaPath = self.home_dir+ "/retina"+im_width+".mat"
         self.coneInvPath = self.home_dir+ "/render_pinv"+im_width+".mat"
-        self.coneInv = torch.from_numpy(sio.loadmat(self.coneInvPath)['render_pinv'], dtype = torch.float32, device = "cuda")
+        self.coneInv = torch.tensor(sio.loadmat(self.coneInvPath)['render_pinv'], dtype = torch.float32, device = "cuda")
 
         self.G = copy.deepcopy(Generator).eval().requires_grad_(False).to(device)
 
