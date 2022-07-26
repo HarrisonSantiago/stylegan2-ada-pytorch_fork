ff1eab425c5c9576452f3e1ee246b60dba3e409b
HarrisonSantiago <harrisonsantiago4@gmail.com>
2022-07-25 17:47:44 -0700

    Today I've been doing stuff with loss fcns

--- ILO_driver.py
+++ ILO_driver.py
@@ -610,7 +610,7 @@ class LatentOptimizer(torch.nn.Module):
 
         loss_tracker = []
 
-        for step in range(200):
+        for step in range(50):
             ws = w_opt.repeat([1, self.G.mapping.num_ws, 1])
 
             img = self.G.synthesis(ws, noise_mode='const', force_fp32=True)
@@ -668,7 +668,7 @@ class LatentOptimizer(torch.nn.Module):
         #loss_fcn1.cuda()
         #ssim_loss = pytorch_ssim.SSIM()
         mse_min = np.inf
-        num_steps = 350
+        num_steps = 50
         ws = ws.detach().clone()
 
         for i in range(0, ws.shape[1]):
