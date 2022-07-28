d7f1af2513c0054f6b16174ece7c248985b3f7e6
HarrisonSantiago <harrisonsantiago4@gmail.com>
2022-07-27 18:35:57 -0700

    Today I've been doing stuff with loss fcns

--- ILO_driver.py
+++ ILO_driver.py
@@ -785,7 +785,8 @@ class LatentOptimizer(torch.nn.Module):
                 print(np.array(top).shape)
 
                 print(np.array(img).shape)
-                video.append_data(np.concatenate([np.transpose(np.array(top),(2,1,0)), np.transpose(np.array(img), (2,1,0))], axis=2))
+                video.append_data(np.concatenate([np.expand_dims(np.transpose(np.array(top),(2,1,0)), axis=0)
+                                                     , np.expand_dims(np.transpose(np.array(img), (2,1,0)), axis=0)], axis=2))
             video.close()
 
         return 0
